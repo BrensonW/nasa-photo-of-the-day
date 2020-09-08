da8b81101563e59b5ef057513a9b5f4105883fff
Brenson <brensonw300@gmail.com>
2020-09-08 16:24:03 -0700

    Working through making site look good

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className = "header">
-            <h1>NASA</h1>
+            <h1>NASA's</h1>
             <h2>Photo Of The Day</h2>
         </div>
     );
