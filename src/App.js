d0597f96a4d9c393c8d01f3161949a3b089a6930
Brenson <brensonw300@gmail.com>
2020-09-08 15:56:13 -0700

    Added files nasapic, photo, header and added code inside them to show a page on npm start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import NasaPic from "./photo/NasaPic.js";
 
 function App() {
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
+      <Header />
+      <NasaPic />
     </div>
   );
 }
